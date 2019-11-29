15001c9d62be83be7aab21047aaadb7043271126
Sunny Peng <sunnyp1227@gmail.com>
2019-11-29 00:36:38 -0800

    add notes on oh-my-zsh setup

--- note/mac-setup/installs.sh
+++ note/mac-setup/installs.sh
@@ -1,3 +1,17 @@
-# install oh-my-zsh
+## install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+## install power-line fonts
+# https://github.com/powerline/fonts
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+
+## update the .zshrc to change the color in prompt_dir for agnoster theme
 
